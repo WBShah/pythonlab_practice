5b319be78d2e26dbaedd265bf28b5110ff6a4e9a
Waqas Shah <wesshah@Waqass-MBP.hub>
2023-06-02 22:07:46 +1000

    Python code updated with more numpy examples

--- python_numpy.py
+++ python_numpy.py
@@ -13,4 +13,43 @@ print(arr)
 # array squared
 arr_squared = arr ** 2
 print (arr_squared)
+# output [ 9 16 25 36 49 ]
 
+
+# Accessing elements in the array
+print(arr[2])
+# output 5
+
+# slicing the array
+print(arr[1:4])
+#output: [4 5 6]
+
+# performing aggregate operations on the array
+arr_sum = np.sum(arr)
+print(arr_sum)
+# output: 25
+
+# Calculate the mean of all elements 
+arr_mean = np.mean(arr)
+print(arr_mean)
+# Output: 5.0
+
+# append array to make it long enough to create a 2x3 matrix 
+value = 8
+arr_append = np.append(arr,value)
+print(arr_append)
+
+
+# reshaing the array
+arr_2d = arr_append.reshape([2,3])
+print(arr_2d)
+
+# creating special type of arrays
+zeros_arr = np.zeros((3,2))
+print(zeros_arr)
+
+ones_arr = np.ones((2,4))
+print(ones_arr)
+
+random_arr = np.random.rand(3,3)
+print(random_arr)
