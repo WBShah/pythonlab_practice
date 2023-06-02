83609169aefa3a770974494d0f46220371fc629b
Waqas Shah <wesshah@Waqass-MacBook-Pro.local>
2023-06-02 16:13:51 +1000

    numpy test code for practice added to the branch

---
+++ python_numpy.py
@@ -0,0 +1,11 @@
+import numpy as np
+
+# creating a numpy array
+arr = np.array([1,2,3,4,5])
+print(arr)
+# output: [1 2 3 4 5]
+
+# Performing mathematical operations on the array - adds +2 for each value
+arr += 2 
+print(arr)
+# output: [3 4 5 6 7]
